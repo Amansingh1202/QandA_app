790674340e36a6cda69083d0e180f46dff65b48d
Amansingh1202 <aks001235@gmail.com>
2020-10-31 18:18:41 +0530

    completed checking for duplicates during signup

--- quiz/views.py
+++ quiz/views.py
@@ -9,9 +9,15 @@ def save(request):
         name = request.POST['name']
         email = request.POST['email']
         password = request.POST['password']
-    newUser = models.User(full_name=name,email=email,password=password)
-    newUser.save()
-    return redirect('login/')
+    user = models.User.objects.filter(email=email).all()
+    if not user:
+        newUser = models.User(full_name=name,email=email,password=password)
+        newUser.save()
+        return redirect('login/')
+    else:
+        args = {}
+        args['error'] = "Email already exists!!"
+        return render(request,'quiz/index.html',args)
 
 def login(request):
     return render(request,'quiz/login.html')
